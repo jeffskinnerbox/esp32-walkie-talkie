7aeace28bc5abee34ac2e79cd271798d87d34bd1
jeffskinnerbox <jeff.irland@verizon.net>
2022-11-18 10:40:57 -0500

    working and stable

---
+++ test/hello-world-1/README.md
@@ -0,0 +1,38 @@
+<!--
+Maintainer:   jeffskinnerbox@yahoo.com / www.jeffskinnerbox.me
+Version:      0.0.1
+-->
+
+
+<div align="center">
+<img src="http://www.foxbyrd.com/wp-content/uploads/2018/02/file-4.jpg" title="These materials require additional work and are not ready for general use." align="center">
+</div>
+
+
+---
+
+# DESCRIPTION
+The most basic of ESP32 test routines.  All that there is to observe is a
+blinking LED on the development board.
+
+# PHYSICAL DESIGN
+
+## Hardware
+ESP-32S (aka ESP-WROOM-32 ESP32) Development Board - https://www.amazon.com/gp/product/B0718T232Z/
+
+## Wiring
+Nothing required
+
+## Software Settings
+Nothing required
+
+# MONITOR
+Observing the blinking LED is the only monitor that can be done.
+
+# TESTING
+None can be done.
+
+# REFERENCE MATERIALS
+
+# SOURCES
+
--- test/hello-world-1/hello-world-1.ino
+++ test/hello-world-1/hello-world-1.ino
@@ -4,29 +4,29 @@
 Maintainer:   jeffskinnerbox@yahoo.com / www.jeffskinnerbox.me
 Version:      0.0.1
 
-DESCRIPTION:
+DESCRIPTION
     The most basic of ESP32 test routines.  All that there is to observe is a
     blinking LED on the development board.
 
-PHYSICAL DESIGN:
+PHYSICAL DESIGN
     Hardware
         ESP-32S (aka ESP-WROOM-32 ESP32) Development Board - https://www.amazon.com/gp/product/B0718T232Z/
 
     Wiring
         Nothing required
 
-MONITOR:
+    Software Settings
+        Nothing required
+
+MONITOR
     Observing the blinking LED is the only monitor that can be done.
 
 TESTING
     None can be done.
 
-REFERENCE MATERIALS:
+REFERENCE MATERIALS
 
-SOURCES:
-
-CREATED BY:
-    jeffskinnerbox@yahoo.com
+SOURCES
 
 ------------------------------------------------------------------------------*/
 
